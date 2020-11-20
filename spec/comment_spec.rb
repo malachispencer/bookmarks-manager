0161091593b632d012dacf4b0faddbf5cd409fe7
Malachi Spencer <malachispencer@MA309.fritz.box>
2020-11-20 13:50:06 +0000

    Added unit test for .create in Comment class to add comment to db

---
+++ spec/comment_spec.rb
@@ -0,0 +1,12 @@
+describe Comment do
+  describe '.create' do
+    let(:bookmark) { double('bookmark', id: 416, title: 'OVO', url: 'http://www.ovosound.com') }
+    let(:comment)  { Comment.create(text: 'Drake is the MJ of this era', bookmark_id: bookmark.id) }
+    let(:comment_from_db) { persisted_data(table: 'comments', id: comment.id) }
+
+    it 'adds a comment to the database' do
+      expect(comment.id).to eq(comment_from_db['id'])
+      expect(comment.text).to eq('Drake is the MJ of this era')
+    end
+  end
+end
