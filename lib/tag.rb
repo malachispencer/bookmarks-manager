922a94b1f596e132f167340eb12ed885bd007095
Malachi Spencer <malachispencer@MA309.fritz.box>
2020-11-21 13:48:44 +0000

    Tag .create method adds tag to db

--- lib/tag.rb
+++ lib/tag.rb
@@ -1,3 +1,18 @@
-class Tag
+require_relative './database_connection.rb'
 
+class Tag
+  attr_reader :id, :content
+
+  def initialize(id:, content:)
+    @id = id
+    @content = content
+  end
+
+  def self.create(content:)
+    result = DatabaseConnection.query(
+      "INSERT INTO tags (content) VALUES ('#{content}') RETURNING id, content;"
+    )
+
+    Tag.new(id: result[0]['id'], content: result[0]['content'])
+  end
 end
--- spec/units/tag_spec.rb
+++ spec/units/tag_spec.rb
@@ -2,10 +2,10 @@ describe Tag do
   describe '.create' do
     it 'adds a tag to the database' do
       bookmark = Bookmark.create(title: 'Goal', url: 'http://www.goal.com')
-      tag = Tag.create(content: 'football', bookmark_id: bookmark.id)
+      tag = Tag.create(content: 'football')
       tag_from_db = persisted_data(table: 'tags', id: tag.id)
 
-      expect(tag.id).to eq(tag_from_db.id)
+      expect(tag.id).to eq(tag_from_db['id'])
       expect(tag.content).to eq('football')
     end
   end
