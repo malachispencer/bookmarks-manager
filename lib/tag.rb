2c9293bd0fb1bf27f9ff4fa6a812e0ea9dff9202
Malachi Spencer <malachispencer@MA309.fritz.box>
2020-11-21 16:42:19 +0000

    .filter method returns all tags for a given Bookmark

--- lib/tag.rb
+++ lib/tag.rb
@@ -15,4 +15,17 @@ class Tag
 
     Tag.new(id: result[0]['id'], content: result[0]['content'])
   end
+
+  def self.filter(bookmark_id:)
+    result = DatabaseConnection.query(
+      "SELECT id, content 
+      FROM bookmarks_tags INNER JOIN tags 
+      ON tags.id = bookmarks_tags.tag_id 
+      WHERE bookmarks_tags.bookmark_id = '#{bookmark_id}';"
+    )
+
+    result.map do |tag|
+      Tag.new(id: tag['id'], content: tag['content'])
+    end
+  end
 end
--- spec/units/tag_spec.rb
+++ spec/units/tag_spec.rb
@@ -9,4 +9,18 @@ describe Tag do
       expect(tag.content).to eq('football')
     end
   end
+
+  describe '.filter' do
+    it 'returns all the tags for the Bookmark(id) passed in' do
+      bookmark = Bookmark.create(title: 'Goal', url: 'http://www.goal.com')
+      tag_one = Tag.create(content: 'football')
+      tag_two = Tag.create(content: 'sports')
+      BookmarkTag.create(tag_id: tag_one.id, bookmark_id: bookmark.id)
+      BookmarkTag.create(tag_id: tag_two.id, bookmark_id: bookmark.id)
+      tags = Tag.filter(bookmark_id: bookmark.id)
+      
+      expect(tags[0].id).to eq(tag_one.id)
+      expect(tags[1].content).to eq(tag_two.content)
+    end
+  end
 end
