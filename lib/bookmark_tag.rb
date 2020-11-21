3e5faa2c12e36e28bf5c41bd41af1eea59b3eefb
Malachi Spencer <malachispencer@MA309.fritz.box>
2020-11-21 14:24:36 +0000

    .create method adds BookmarkTag to join table in db

--- lib/bookmark_tag.rb
+++ lib/bookmark_tag.rb
@@ -1,3 +1,21 @@
-class BookmarkTag
+require_relative './database_connection.rb'
 
+class BookmarkTag
+  attr_reader :tag_id, :bookmark_id
+
+  def initialize(tag_id:, bookmark_id:)
+    @tag_id = tag_id
+    @bookmark_id = bookmark_id
+  end
+
+  def self.create(tag_id:, bookmark_id:)
+    result = DatabaseConnection.query(
+      "INSERT INTO bookmarks_tags (tag_id, bookmark_id) VALUES ('#{tag_id}', '#{bookmark_id}') RETURNING tag_id, bookmark_id;"
+    )
+
+    BookmarkTag.new(
+      tag_id: result[0]['tag_id'],
+      bookmark_id: result[0]['bookmark_id']
+    )
+  end
 end
--- spec/units/bookmark_tag_spec.rb
+++ spec/units/bookmark_tag_spec.rb
@@ -4,9 +4,7 @@ describe BookmarkTag do
       bookmark = Bookmark.create(title: 'Goal', url: 'http://www.goal.com')
       tag = Tag.create(content: 'football')
       bookmark_tag = BookmarkTag.create(bookmark_id: bookmark.id, tag_id: tag.id)
-      bookmark_tag_from_db = persisted_data(table: bookmarks_tags, id: bookmark_tag.id)
-
-      expect(bookmark_tag.id).to eq(bookmark_tag_from_db['id'])
+      
       expect(bookmark_tag.bookmark_id).to eq(bookmark.id)
       expect(bookmark_tag.tag_id).to eq(tag.id)
     end
