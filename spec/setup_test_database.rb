aa98d191fa159ddaae02d6749c3ee2c0767f56bc
Malachi Spencer <malachispencer@MA309.fritz.box>
2020-11-21 12:50:30 +0000

    setup_test_database truncates tags and bookmarks_tags tables

--- spec/setup_test_database.rb
+++ spec/setup_test_database.rb
@@ -1,6 +1,4 @@
-require 'pg'
-
 def setup_test_database
   connection = PG.connect(dbname: 'bookmarks_manager_test')
-  connection.exec('TRUNCATE bookmarks, comments;')
+  connection.exec('TRUNCATE bookmarks, comments, tags, bookmarks_tags;')
 end
