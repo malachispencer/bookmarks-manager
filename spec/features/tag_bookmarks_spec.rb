4043d83ed63d1ce9a2c0c5148bda190de4a1613c
Malachi Spencer <malachispencer@MA309.fritz.box>
2020-11-22 10:50:52 +0000

    Added feature test for viewing bookmarks by tag

--- spec/features/tag_bookmarks_spec.rb
+++ spec/features/tag_bookmarks_spec.rb
@@ -14,3 +14,32 @@ feature 'add tag to bookmark' do
     expect(first('.bookmark')).to have_content('football')
   end
 end
+
+feature 'filter bookmarks by tag' do
+  scenario 'user can see all bookmarks with a certain tag' do
+    bm_one = Bookmark.create(title: 'Goal', url: 'http://www.goal.com')
+    bm_two = Bookmark.create(title: 'OVO', url: 'http://www.ovosound.com')
+    bm_three = Bookmark.create(title: 'XO', url: 'http://www.theweeknd.com')
+
+    visit('/bookmarks')
+
+    within page.find('.bookmark:nth-of-type(2)') do
+      click_button('Add Tag')
+    end
+    fill_in('tag', with: 'toronto')
+    click_button('Submit')
+
+    within page.find('.bookmark:nth-of-type(3)') do
+      click_button('Add Tag')
+    end
+    fill_in('tag', with: 'toronto')
+    click_button('Submit')
+
+    within page.find('.bookmark:nth-of-type(2)') do
+      click_link('toronto')
+    end
+
+    expect(page).to have_link('OVO', href: 'http://www.ovosound.com')
+    expect(page).to have_link('XO', href: 'http://www.theweeknd.com')
+  end
+end
