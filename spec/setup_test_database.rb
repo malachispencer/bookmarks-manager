88939c7d4f1527d91037de63509664f64846774e
Malachi Spencer <malachispencer@MA309.fritz.box>
2020-11-22 15:47:45 +0000

    Added user table to database

--- spec/setup_test_database.rb
+++ spec/setup_test_database.rb
@@ -1,4 +1,4 @@
 def setup_test_database
   connection = PG.connect(dbname: 'bookmarks_manager_test')
-  connection.exec('TRUNCATE bookmarks, comments, tags, bookmarks_tags;')
+  connection.exec('TRUNCATE bookmarks, comments, tags, bookmarks_tags, users;')
 end
