c2b9b067be0c3a0fde7ae401ee3a90bd315c33f4
Malachi Spencer <malachispencer@MA309.fritz.box>
2020-11-23 19:16:10 +0000

    Added feature test for authentication

---
+++ spec/features/authentication_spec.rb
@@ -0,0 +1,14 @@
+feature 'authentication' do
+  scenario 'user can sign in' do
+    User.create(name: 'mjss', email: 'm.spencer@makers.com', password: 'p20201122')
+
+    visit('/')
+    click_link('Sign In')
+    fill_in('name', with: 'mjss')
+    fill_in('email', with: 'm.spencer@makers.com')
+    fill_in('password', with: 'p20201122')
+    click_button('Sign In')
+
+    expect(page).to have_content('Hello, Malachi')
+  end
+end
